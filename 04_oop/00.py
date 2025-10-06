f7fb6529ec3e2a330d450aa5a540a38379be21d4
Anonymoussingh1 <aman.bhamra@strathmore.edu>
2025-10-06 14:05:44 +0300

    python

---
+++ 04_oop/00.py
@@ -0,0 +1,11 @@
+class book:
+
+    def__init__(self, author, title):
+        self.author = author
+        self.title = title
+
+        def getDetails(self):
+            print(f"The author is{self.author}")
+
+# Creating an Object
+java = book("Java Author","Java Book")
